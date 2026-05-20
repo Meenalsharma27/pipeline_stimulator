26e66dd1e8978d2f61ba522f2628aa011e8dfef2
Afreen Khanam <afreenkhanam386@gmail.com>
2026-05-21 00:58:43 +0530

    Revert guideline changes and script

--- update_logs.ps1
+++
@@ -1,9 +0,0 @@
-git pull
-
-git log --all --pretty=format:"%h | %an | %ad | %s" --date=short > commit_logs.txt
-
-git add commit_logs.txt
-
-git commit -m "docs: update team commit logs"
-
-git push
