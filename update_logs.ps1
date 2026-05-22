dbf7aaf3e7513ba30bdfe49d940da39ed42c7ea9
Afreen Khanam <afreenkhanam386@gmail.com>
2026-05-22 09:28:14 +0530

    R25SK000: Update team commit logs

--- update_logs.ps1
+++ update_logs.ps1
@@ -2,12 +2,10 @@ git switch Demo
 
 git pull origin Demo
 
-git log --all --pretty=format:"%h | %an | %ad | %s" --date=short > commit_logs.txt
+git log --all --pretty=format:"%h | %an | %ad | %s" --date=short | Out-File -FilePath commit_logs.txt -Encoding utf8
 
 git add commit_logs.txt commit_guidelines.md update_logs.ps1
 
 git commit -m "R25SK000: Update team commit logs"
 
 git push origin Demo
-
-
